734263f435ee469895954c36d301c0a1a49c7aff
DeneIs <55320251+A00433151@users.noreply.github.com>
2020-02-14 01:55:01 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -10,7 +10,7 @@ import re
 import csv
 
 list_new=[]
-input_file=open("/Users/denes/Downloads/workshop_2/data/apache_log.txt")              # Change this path to your working directory with data file
+input_file=open("./apache_log.txt")              # Change this path to your working directory with data file
 for line in input_file.readlines():
     pattern1 = r'^\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
     pattern2 = r'[\d]{1,2}/[ADFJMNOS|adfjmnos]\w*/[\d]{4}:[\d]{1,2}:[\d]{1,2}:[\d]{1,2}[\s][+|-][\d]{4}'
@@ -19,7 +19,7 @@ for line in input_file.readlines():
     ip_corrected = str(ip)[2:-2]                                            # Removed square brackets and quotes at start and end of list 
     timestamp_corrected = str(timestamp)[2:-2]                              # Removed square brackets and quotes at start and end of list 
     list_new.append([ip_corrected, timestamp_corrected])
-with open("/Users/denes/Downloads/workshop_2/data/apache_log_export.csv", 'w', newline='') as export_file:         # Change this path to your working directory where the export file is saved
+with open("./apache_log_export.csv", 'w', newline='') as export_file:         # Change this path to your working directory where the export file is saved
     for i in list_new:
         writer = csv.writer(export_file)
         writer.writerow(i)
